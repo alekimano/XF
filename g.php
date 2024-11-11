6b9c80584c98db60bc3fa308c3d529fecb3b9809
alekimano <alekimano@icloud.com>
2024-11-10 23:08:05 -0800

    Add files via upload

---
+++ g.php
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import logging
+
+app = Flask(__name__)
+
+# Настройка логирования
+logging.basicConfig(filename='post_requests.log', level=logging.INFO)
+
+@app.route('/bitrix/admin/', methods=['POST'])
+def log_post_request():
+    # Логируем информацию о запросе
+    logging.info('New POST request received')
+    
+    # Логируем данные запроса
+    logging.info(f'Headers: {request.headers}')
+    logging.info(f'Body: {request.form}')
+
+    # Возвращаем успешный ответ
+    return 'Request logged', 200
+
+if __name__ == '__main__':
+    app.run(port=5000)
