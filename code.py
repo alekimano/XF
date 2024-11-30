dec8d8f96c48058bb71f8801c243119d2d4448dd
alekimano <alekimano@icloud.com>
2024-11-30 19:19:26 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,21 +1,20 @@
-   from flask import Flask, request
-   import logging
+from flask import Flask, request
+import logging
 
-   app = Flask(__name__)
+app = Flask(__name__)
 
-   # Настройка логирования
-   logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
+# Настройка логирования
+logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
 
-   @app.route('/', methods=['GET'])
-   def index():
-       log_request()
-       return "Hello, World!"
+@app.route('/', methods=['GET'])
+def index():
+    log_request()
+    return "Hello, World!"
 
-   def log_request():
-       ip = request.remote_addr
-       url = request.url
-       logging.info(f'GET request from {ip} to {url}')
+def log_request():
+    ip = request.remote_addr
+    url = request.url
+    logging.info(f'GET request from {ip} to {url}')
 
-   if __name__ == '__main__':
-       app.run(host='0.0.0.0', port=5000)
-   
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
