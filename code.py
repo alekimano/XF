96ad42d4bb03ed6e300164eda3c7d9cac18da0d5
alekimano <alekimano@icloud.com>
2024-11-30 19:18:06 +0300

    Update and rename code.php to code.py

---
+++ code.py
@@ -0,0 +1,21 @@
+   from flask import Flask, request
+   import logging
+
+   app = Flask(__name__)
+
+   # Настройка логирования
+   logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
+
+   @app.route('/', methods=['GET'])
+   def index():
+       log_request()
+       return "Hello, World!"
+
+   def log_request():
+       ip = request.remote_addr
+       url = request.url
+       logging.info(f'GET request from {ip} to {url}')
+
+   if __name__ == '__main__':
+       app.run(host='0.0.0.0', port=5000)
+   
