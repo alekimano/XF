f54338e076b1eda90675eaa64ea1013ad832c349
alekimano <alekimano@icloud.com>
2024-12-12 13:51:25 +0300

    Update g.php

--- g.php
+++ g.php
@@ -1,22 +1,50 @@
-from flask import Flask, request
-import logging
-
-app = Flask(__name__)
-
-# Настройка логирования
-logging.basicConfig(filename='post_requests.log', level=logging.INFO)
-
-@app.route('/home/bitrix/www/bitrix/admin/', methods=['POST'])
-def log_post_request():
-    # Логируем информацию о запросе
-    logging.info('New POST request received')
-    
-    # Логируем данные запроса
-    logging.info(f'Headers: {request.headers}')
-    logging.info(f'Body: {request.form}')
-
-    # Возвращаем успешный ответ
-    return 'Request logged', 200
-
-if __name__ == '__main__':
-    app.run(port=5000)
+<?php
+return [
+ 'sberbank' => [
+        //'username' => 'p1655455610-api',
+	'username' => '99BALLOV',
+        //'password' => 'jfk2laxgs1998',
+	'password' => 'H5JZk6F:SvNWY5581AblcgmJt',
+    ],
+	'sberbankInstallment' => [
+        	'token' => 'k2t07q7e4vnonse9fdeq4osdbj',
+        	'callback_token' => 'ivsa54ql7g2rn0n67c6297bo2g',
+    ],
+    'cloudPayment' => [
+            'protect' => 'Vs82bvis7bfu', //protect key
+       	    'pk' => 'pk_743f8f42e2bc337ca17d2727efbb7',
+    	    'apiSecret' => '48bcec9f9742bf868b8779c28fb5e108',
+    ],
+    'sbpClient' => [
+        'id' => '8aac6aef-ede3-42d6-b924-90a0192291f5',
+        'secret' => '0c850d09-a41f-4d0a-a4e1-1316a139d233',
+        'memberId' => "00004953",
+        'idQr' => "32399410",
+ 	'sbpMemberId' => "100000000111"
+    ],
+    'tbankInstallment' => [
+        'shopId' => 'a085a44d-ca5d-4a34-a3ef-6bd6ada7dcf8',
+        'showcaseId' => 'cc9de435-1701-4c2b-9cba-25e4776a5c13',
+        'apiPassword' => '6vvboeSWRu'
+    ],
+    'admitad' => [
+        'campaign_code' => '6a90217d62',
+        'postback_key' => 'f1a79aa3C25d4A745D553A9e213944Ab',
+    ],
+    'edpartners' => [
+        'secure' => '409e2bca'
+    ],
+    'cityAds' => [
+        'secure' => '0b8Jjo',
+    ],
+    'advertise' => [
+        'token' => '3f1c0da904d470534baf7e03f14fad7d',
+    ],
+    'yandexPayMerchant' => 'fda0305095d84408b2527f1ad56a4b72.tYQ8g-mi579Bi41BIDz5I7xsrJuUk_JL',
+    'evotor' => [
+        'login' => 'ncDJMYlFti',
+        'pass' => 'ltwAIxxXLwnYlbF',
+        'group_code' => 'dadc46ad-fb56-4952-8485-760dc655cab5',
+    ],
+    'googleMacros' => 'AKfycbw9Xn2cI67ue5Q3mfdyoZsSDFKnx3MTTTbNScfZgp9x74VEQXT6jN3KMRVu1bXoP-Cp',
+];
