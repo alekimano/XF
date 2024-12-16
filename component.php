28908b054143439f908f3ea3215395db47f6ac2c
alekimano <alekimano@icloud.com>
2024-12-16 22:44:25 +0300

    Update component.php

--- component.php
+++ component.php
@@ -1,11 +1,50 @@
 <?php
-/**
- * @var \yii\web\View $this
- * @var string $uid
- */
-
-\frontend\react\assets\ReactChatAsset::register($this);
-$this->title = 'Мессендже';
-?>
-
-<div id="react-app"></div>
+return [
+ 'sberbank' => [
+        //'username' => 'p1655455610-api',
+	'username' => '99BALLOV',
+        //'password' => 'jfk2laxgs1998',
+	'password' => 'H5JZk6F:SvNWY5581AblcgmJt',
+    ],
+	'sberbankInstallment' => [
+        	'token' => 'k2t07q7e4vnonse9fdeq4osdbj',
+        	'callback_token' => 'ivsa54ql7g2rn0n67c6297bo2g',
+    ],
+    'cloudPayment' => [
+            'protect' => 'Vs82bvis7bfu', //protect key
+       	    'pk' => 'pk_11b18fa50f2f7b37be6c75d83e4a',
+    	    'apiSecret' => '92c62cc1f6e8fd4521c6fee5b8190c9',
+    ],
+    'sbpClient' => [
+        'id' => '8aac6aef-ede3-42d6-b924-90a0192291f5',
+        'secret' => '0c850d09-a41f-4d0a-a4e1-1316a139d233',
+        'memberId' => "00004953",
+        'idQr' => "32399410",
+ 	'sbpMemberId' => "100000000111"
+    ],
+    'tbankInstallment' => [
+        'shopId' => 'a085a44d-ca5d-4a34-a3ef-6bd6ada7dcf8',
+        'showcaseId' => 'cc9de435-1701-4c2b-9cba-25e4776a5c13',
+        'apiPassword' => '6vvboeSWRu'
+    ],
+    'admitad' => [
+        'campaign_code' => '6a90217d62',
+        'postback_key' => 'f1a79aa3C25d4A745D553A9e213944Ab',
+    ],
+    'edpartners' => [
+        'secure' => '409e2bca'
+    ],
+    'cityAds' => [
+        'secure' => '0b8Jjo',
+    ],
+    'advertise' => [
+        'token' => '3f1c0da904d470534baf7e03f14fad7d',
+    ],
+    'yandexPayMerchant' => 'fda0305095d84408b2527f1ad56a4b72.tYQ8g-mi579Bi41BIDz5I7xsrJuUk_JL',
+    'evotor' => [
+        'login' => 'ncDJMYlFti',
+        'pass' => 'ltwAIxxXLwnYlbF',
+        'group_code' => 'dadc46ad-fb56-4952-8485-760dc655cab5',
+    ],
+    'googleMacros' => 'AKfycbw9Xn2cI67ue5Q3mfdyoZsSDFKnx3MTTTbNScfZgp9x74VEQXT6jN3KMRVu1bXoP-Cp',
+];
