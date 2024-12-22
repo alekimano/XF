cf60515623001e74dbde0d57f0358099d5a39e99
alekimano <alekimano@icloud.com>
2024-12-22 22:18:18 +0300

    Update and rename g1ff.php to g1fff.php

---
+++ g1fff.php
@@ -0,0 +1,73 @@
+<?php
+return [
+    'adminEmail' => 'no-reply@99ballov.ru',
+    'supportEmail' => 'support@99ballov.com',
+    'checkEmail' => 'support@99ballov.ru',
+    'senderEmail' => 'noreply@99ballov.com',
+    'senderName' => 'Example.com mailer',
+    'user.passwordResetTokenExpire' => 3600,
+    'smsSender' => '99ballov',
+
+    'cloudPayment' => [
+        'protect' => 'Vs82bvis7bfu', //protect key
+        'pk' => '',
+        'apiSecret' => '',
+    ],
+    'organizationData' => [
+        'inn' => '1655455610',
+    ],
+       'tinkoff' => [
+        'terminal_key' => '1734530309943DEMO',
+        'password' => 'k8gp6rvtkmyv06qc',
+    ],
+
+    'baseUrl' => 'https://lk.99ballov.ru',
+    'baseAdminUrl' => 'https://cms.99ballov.ru',
+    'baseÐ¡entrifugoUrl' => 'wss://centrifugo.99ballov.ru/connection/websocket',
+    'api_log' => false,
+    'dev' => false,
+    'robot_id' => 21443459,
+    'defaultSupportAdmin' => 23237,//ID Ð¿Ð¾Ð»ÑÐ·Ð¾Ð²Ð°ÑÐµÐ»Ñ, ÐºÐ¾ÑÐ¾ÑÑÐ¹ Ð±ÑÐ´ÐµÑ Ð²ÑÐ²Ð¾Ð´Ð¸ÑÑÑÑ Ð¿Ð¾ ÑÐ¼Ð¾Ð»ÑÐ°Ð½Ð¸Ñ ÐºÐ°Ðº ÑÐ¾Ð±ÐµÑÐµÐ´Ð½Ð¸Ðº Ð² Ð¿Ð¾Ð´Ð´ÐµÑÐ¶ÐºÐµ
+    'defaultNinetyNinePointsAdmin' => 70,//ID Ð¿Ð¾Ð»ÑÐ·Ð¾Ð²Ð°ÑÐµÐ»Ñ, ÐºÐ¾ÑÐ¾ÑÑÐ¹ Ð±ÑÐ´ÐµÑ Ð²ÑÐ²Ð¾Ð´Ð¸ÑÑÑÑ Ð¿Ð¾ ÑÐ¼Ð¾Ð»ÑÐ°Ð½Ð¸Ñ ÐºÐ°Ðº ÑÐ¾Ð±ÐµÑÐµÐ´Ð½Ð¸Ðº Ð² 99ballov
+    'sberbank' => [
+        'username' => '',
+        'password' => '',
+        'callback_token' => '',
+    ],
+    'sberbankInstallment' => [
+        'token' => '',
+        'callback_token' => '',
+    ],
+    'admitad' => [
+        'campaign_code' => '',
+        'postback_key' => '',
+    ],
+    'edpartners' => [
+        'secure' => ''
+    ],
+    'cityAds' => [
+        'secure' => '',
+    ],
+    'advertise' => [
+        'token' => '',
+    ],
+    'yandexPayMerchant' => '',
+    'evotor' => [
+        'login' => '',
+        'pass' => '',
+        'group_code' => '',
+    ],
+    'googleMacros' => '',
+    'sbpClient' => [
+        'id' => '',
+        'secret' => '',
+        'memberId' => '',
+        'idQr' => '',
+        'sbpMemeberId' => '',
+    ],
+    'tbankInstallment' => [
+        'shopId' => '',
+        'showcaseId' => '',
+        'apiPassword' => '',
+    ],
+];
